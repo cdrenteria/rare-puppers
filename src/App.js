f9912315a8c1429f274be216c23cc456010cf6a7
Chandler Renteria <69171143+cdrenteria@users.noreply.github.com>
2022-08-14 10:14:51 -0600

    Reformatted and began adding react-router-dom
    adding react-router-dom in order to navigate to a details page for a specific adoptable dog

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import NavBar  from './components/NavBar';
-import Hero from './components/Hero'
-import Card from './components/Card'
-import Data from './components/data'
-import './App.css';
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
+import Card from "./components/Card";
+import Data from "./components/data";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  console.log(Data)
-  const cards = Data.map(property => {
-    return(
-      <Card 
+  const cards = Data.map((property) => {
+    return (
+      <Card
+        key={Math.floor(Math.random() * 100)}
         img={property.img}
-        milesAway ={property.milesAway}
+        milesAway={property.milesAway}
         country={property.country}
         disability={property.disability}
         cost={property.cost}
-      /> 
-    )
-  })
+      />
+    );
+  });
+  console.log(cards);
   return (
     <div className="App">
       <NavBar />
       <Hero />
-      <div className="cards-list">
-        {cards}
-      </div>
+      <div className="cards-list">{cards}</div>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,17 @@
 import React from "react";
-import carIcon from '../images/car-icon.svg'
+import carIcon from "../images/car-icon.svg";
 
 export default function Card(props) {
-    return(
-        <div className="animal-card">
-            <img className="animal-img" src={"../images/" + props.img} />
-            <div className="location-information">
-                <img className="icon" src={carIcon}></img>
-                <p className="miles-away">{props.milesAway} mi</p>
-                <p className="country">{props.country}</p>
-            </div>
-            <p className="disabilityTags">({props.disability})</p>
-            <h4 className="estimatedMonthlyCost">Around ${props.cost}</h4>
-        </div>
-    )
+  return (
+    <div className="animal-card">
+      <img className="animal-img" src={"../images/" + props.img} alt="dog" />
+      <div className="location-information">
+        <img className="icon" src={carIcon} alt="car icon" />
+        <p className="miles-away">{props.milesAway} mi</p>
+        <p className="country">{props.country}</p>
+      </div>
+      <p className="disabilityTags">({props.disability})</p>
+      <h4 className="estimatedMonthlyCost">Around ${props.cost}</h4>
+    </div>
+  );
 }
