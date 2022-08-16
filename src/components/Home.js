d652ede1fe4287a0cbcdca03321425e420594101
Chandler Renteria <69171143+cdrenteria@users.noreply.github.com>
2022-08-16 15:00:31 -0600

    first api call
    +got first response from rescue pets api
    +a 500 error on their part is preventing further development with special needs animals
    + I am focusing instead on how to handle the information when it is received and will adjust the call when rescuepets can fix their api

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,27 @@
-
 import Hero from "./Hero";
 import Data from "./data";
 import Card from "./Card";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 export default function Home() {
+  fetch(
+    "https://api.rescuegroups.org/v5/public/animals/search/isneedingfoster/?limit=1",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "nlPUcL9J",
+      },
+      body: JSON.stringify({
+        filterRadius: {
+          miles: 25,
+          postalcode: 84105,
+        },
+      }),
+    }
+  )
+    .then((res) => res.json())
+    .then((data) => console.log(data));
   const cards = Data.map((property) => {
     return (
       <Link to={`details/${property.id}`}>
